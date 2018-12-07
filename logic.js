2a661b4cc8e8f2ab01192d825d7068227f48e0d0
Mike <michael.schulman88@gmail.com>
2018-12-07 15:35:11 +0200

    bootstrap

--- logic.js
+++ logic.js
@@ -1,16 +1,17 @@
 class App extends React.Component {
 
-    render () {
+    render() {
         return (
-            <div className = "container">
-                <h1 className = "title">Welcome to your To-do List</h1>
-                <Input/>
+            <div className="container">
+                <NavBar />
+                <h1 className="title">Welcome to your To-do List</h1>
+                <Input />
             </div>
         )
     }
 }
 class Input extends React.Component {
-    constructor (props){
+    constructor(props) {
         super(props)
         this.eventCreate = this.eventCreate.bind(this);
         this.moveTodone = this.moveTodone.bind(this);
@@ -22,7 +23,7 @@ class Input extends React.Component {
         this.hydrateStateWithLocalStorage = this.hydrateStateWithLocalStorage.bind(this);
         this.newItemArray = [];
         this.doneItemArray = [];
-        this.state = { 
+        this.state = {
             todoitems: [],
             doneitems: [],
             isWinter: "",
@@ -32,119 +33,119 @@ class Input extends React.Component {
     saveStateToLocalStorage() {
         // for every item in React state
         for (let key in this.state) {
-          // save to localStorage
-          localStorage.setItem(key, JSON.stringify(this.state[key]));
+            // save to localStorage
+            localStorage.setItem(key, JSON.stringify(this.state[key]));
         }
-      }
-    eventCreate () {
+    }
+    eventCreate() {
         this.newItemArray = this.state.todoitems;
         var activities = this.textInput.value + this.dateInput.value;
         this.newItemArray.push(activities);
-        this.setState ({
+        this.setState({
             todoitems: this.newItemArray
         })
     }
-    moveTodone (e) {
+    moveTodone(e) {
         var text = (e.target.value);
         var newTodo = this.state.todoitems;
         var selectedIndex = newTodo.indexOf(text);
         newTodo.splice(selectedIndex, 1);
         var newCompleted = this.state.doneitems;
         newCompleted.push(text);
-        this.setState ({
+        this.setState({
             doneitems: newCompleted,
             todoitems: newTodo
         })
     }
-    returnTodo (e) {
+    returnTodo(e) {
         var text = (e.target.value);
         var newDone = this.state.doneitems;
         var selectedIndex = newDone.indexOf(text)
         newDone.splice(selectedIndex, 1);
         var notYetdone = this.state.todoitems;
         notYetdone.push(text);
-        this.setState ({
+        this.setState({
             todoitems: notYetdone,
             doneitems: newDone
         })
     }
-    deleteTodo (e) {
+    deleteTodo(e) {
         var listItem = (e.target.value);
         var toDo = this.state.todoitems;
         var selectedIndex = toDo.indexOf(listItem);
         toDo.splice(selectedIndex, 1);
-        this.setState ({
+        this.setState({
             todoitems: toDo
         })
     }
-    deleteDone (e) {
+    deleteDone(e) {
         var listItem = (e.target.value);
         var done = this.state.doneitems;
         var selectedIndex = done.indexOf(listItem);
         done.splice(selectedIndex, 1);
-        this.setState ({
+        this.setState({
             doneitems: done
         })
     }
-    prependFavourite (e) {
+    prependFavourite(e) {
         var starredItem = (e.target.value);
         var toDo = this.state.todoitems;
         var selectedIndex = toDo.indexOf(starredItem);
         toDo.prepend(selectedIndex);
-        this.setState ({
+        this.setState({
             todoitems: toDo
         })
     }
     hydrateStateWithLocalStorage() {
         // for all items in state
         for (let key in this.state) {
-          // if the key exists in localStorage
-          if (localStorage.hasOwnProperty(key)) {
-            // get the key's value from localStorage
-            let value = localStorage.getItem(key);
-    
-            // parse the localStorage string and setState
-            try {
-              value = JSON.parse(value);
-              this.setState({ [key]: value });
-            } catch (e) {
-              // handle empty string
-              this.setState({ [key]: value });
+            // if the key exists in localStorage
+            if (localStorage.hasOwnProperty(key)) {
+                // get the key's value from localStorage
+                let value = localStorage.getItem(key);
+
+                // parse the localStorage string and setState
+                try {
+                    value = JSON.parse(value);
+                    this.setState({ [key]: value });
+                } catch (e) {
+                    // handle empty string
+                    this.setState({ [key]: value });
+                }
             }
-          }
         }
-      }
-      componentDidMount() {
+    }
+    componentDidMount() {
         this.hydrateStateWithLocalStorage();
-    
+
         // add event listener to save state to localStorage
         // when user leaves/refreshes the page
         window.addEventListener(
-          "beforeunload",
-          this.saveStateToLocalStorage.bind(this)
+            "beforeunload",
+            this.saveStateToLocalStorage.bind(this)
         );
-      }
-      componentWillUnmount() {
+    }
+    componentWillUnmount() {
         window.removeEventListener(
-          "beforeunload",
-          this.saveStateToLocalStorage.bind(this)
+            "beforeunload",
+            this.saveStateToLocalStorage.bind(this)
         );
-    
+
         // saves if component has a chance to unmount
         this.saveStateToLocalStorage();
     }
-    render () {
+    render() {
         return (
             <div>
-                <h3 className = "todoTitle">What do you need to get done: </h3>
+                <h3 className="todoTitle">What do you need to get done: </h3>
                 <div className="inputFields">
-                    <input className="input" placeholder="enter activity" type = "text" ref = {input => { this.textInput = input;}}/>
-                    <input className="input" type = "date" ref = {input => { this.dateInput = input;}}/>
-                    <input type = "submit" onClick = {this.eventCreate}/>
+                    <input className="input" placeholder="enter activity" type="text" ref={input => { this.textInput = input; }} />
+                    <input className="input" type="date" ref={input => { this.dateInput = input; }} />
+                    <input type="submit" onClick={this.eventCreate} />
                 </div>
                 <div className="listContainer">
-                    <TodoListItem className="todo" newtodo = {this.state.todoitems} handleClick = {this.moveTodone} handleDelete = {this.deleteTodo} handeFavourite = {this.prependFavourite}/>
-                    <DoneListItem className="done" newdone = {this.state.doneitems} handleClick = {this.returnTodo} handleDonedelete = {this.deleteDone}/>
+                    <TodoListItem className="todo" newtodo={this.state.todoitems} handleClick={this.moveTodone} handleDelete={this.deleteTodo} handeFavourite={this.prependFavourite} />
+                    <DoneListItem className="done" newdone={this.state.doneitems} handleClick={this.returnTodo} handleDonedelete={this.deleteDone} />
                 </div>
             </div>
         );
@@ -152,24 +153,24 @@ class Input extends React.Component {
 }
 class TodoListItem extends React.Component {
     constructor(props) {
-        super (props)
+        super(props)
         this.markDone = this.markDone.bind(this);
         this.deleteItem = this.deleteItem.bind(this);
         this.favouriteItem = this.favouriteItem.bind(this);
     }
-    markDone (e) {
+    markDone(e) {
         this.props.handleClick(e);
     }
-    deleteItem (e) {
+    deleteItem(e) {
         this.props.handleDelete(e)
     }
-    favouriteItem (e) {
+    favouriteItem(e) {
         this.props.handeFavourite(e)
     }
 
-    render () {
+    render() {
         var items = this.props.newtodo.map(
-            (item) => <span key = {item + "todo"}><li key={item + "listitem"}>{item}</li><button type = "button" key={item + "done-button"} value={item} onClick = {this.markDone}>done</button><button type = "button" key = {item + "delete-todo"} value = {item} onClick = {this.deleteItem}>Delete</button><button type = "button" key = {"delete" + item} value = {item} onClick = {this.favouriteItem}>Favourite</button></span>
+            (item) => <span key={item + "todo"}><li key={item + "listitem"}>{item}</li><button type="button" key={item + "done-button"} value={item} onClick={this.markDone}>done</button><button type="button" key={item + "delete-todo"} value={item} onClick={this.deleteItem}>Delete</button><button type="button" key={"delete" + item} value={item} onClick={this.favouriteItem}>Favourite</button></span>
         )
         return (
             <ol>
@@ -180,34 +181,60 @@ class TodoListItem extends React.Component {
 }
 class DoneListItem extends React.Component {
     constructor(props) {
-        super (props)
+        super(props)
         this.markNotdone = this.markNotdone.bind(this);
         this.deleteDoneitem = this.deleteDoneitem.bind(this)
     }
-    markNotdone (e) {
+    markNotdone(e) {
         this.props.handleClick(e);
     }
-    deleteDoneitem (e) {
+    deleteDoneitem(e) {
         this.props.handleDonedelete(e);
     }
 
-    render () {
+    render() {
         var items = this.props.newdone.map(
-            (item) => <span key = {item + "done"}>Done<li key={item}>{item}</li><button type = "button" key={item + "button"} onClick = {this.markNotdone} value = {item}>not done</button><button type = "button" key = {item + "delete-completed"} value = {item} onClick = {this.deleteDoneitem}>Delete</button></span>
+            (item) => <span key={item + "done"}>Done<li key={item}>{item}</li><button type="button" key={item + "button"} onClick={this.markNotdone} value={item}>not done</button><button type="button" key={item + "delete-completed"} value={item} onClick={this.deleteDoneitem}>Delete</button></span>
         )
         return (
-            <ol className = "done">
+            <ol className="done">
                 {items}
             </ol>
         )
     }
 }
 
-function render () {
+class NavBar extends React.Component {
+    render() {
+        return (
+            <nav className="navbar navbar-expand-lg navbar-light bg-light">
+                <a className="navbar-brand" href="#">
+                <img src="http://www.pngmart.com/files/6/Peregrine-Falcon-PNG-Free-Download.png" className="d-inline-block align-top icon" alt=""/>
+                    Falcon List
+                </a>
+                <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+                    <span className="navbar-toggler-icon"></span>
+                </button>
+                <div className="collapse navbar-collapse" id="navbarNavDropdown">
+                    <ul className="navbar-nav td-navbar">
+                        <li className="nav-item active">
+                            <a className="nav-link" href="#">Marked Important <span className="sr-only">(current)</span></a>
+                        </li>
+                        <li className="nav-item">
+                            <a className="nav-link" href="#">About</a>
+                        </li>
+                    </ul>
+                </div>
+            </nav>
+        )
+    }
+}
+
+function render() {
     ReactDOM.render(
-        <App/>,
+        <App />,
         document.getElementById("root")
     );
 }
 
-render ()
+render()
